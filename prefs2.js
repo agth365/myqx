e9d8468a132c45b23356ba4aaa013164ff7899f1
agth365 <41073359+agth365@users.noreply.github.com>
2022-12-04 13:13:30 +0000

    xf

--- prefs2.js
+++ prefs2.js
@@ -1,5 +1,6 @@
 console.log('p21')
 $prefs.setValueForKey('p1.a', 'a11')
 const x = $prefs.valueForKey('p1.a')
-$notify('主标题p2', '副标题2', 'p1.a:' + x)
+x = 'cc'
+$notify('主标题p2', '副标题2', 'p1.a2:' + x)
 console.log('p22')
