60227408e6e5937a0c9d495f5b61d32cf5c49912
LAPTOP-2RO3BO9E\z <agth365@gmail.com>
2022-12-04 21:47:16 +0800

    2324

--- prefs2.js
+++ prefs2.js
@@ -1,7 +1,7 @@
 console.log('p21')
-$prefs.setValueForKey('p1.a', 'a11')
 let x = $prefs.valueForKey('a11')
+if (!x) {
+    x = 'default'
+}
 $notify('主标题p23', '副标题2', 'p1.a2:' + x)
-x = 'cc'
-$notify('主标题p2', '副标题2', 'p1.a2:' + x)
 console.log('p22')
