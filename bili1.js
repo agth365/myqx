c4db260bc584d08d6f59f1f728cbed1c05eb9766
LAPTOP-2RO3BO9E\z <agth365@gmail.com>
2022-12-04 20:20:18 +0800

    1

--- bili1.js
+++ bili1.js
@@ -1,1 +1,19 @@
 console.log('abc')
+/**
+ *
+ * var body = $response.body;
+ * var obj = JSON.parse(body);
+ *
+ * obj['result'] = 0;
+ * body = JSON.stringify(obj);
+ *
+ * console.log(body);
+ *
+ * $done(body);
+ *
+ *  $notify
+ *
+ *  The $prefs is for persistent store: $prefs.valueForKey(key), $prefs.setValueForKey(value, key), $prefs.removeValueForKey(key), $prefs.removeAllValues().
+ */
+
+
